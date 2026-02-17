1500424b2ef20bffbf1e3f6ee383afde25279e62
MIMS1302 <maria.mendez16@uamcr.net>
2026-02-16 18:56:32 -0600

    Fix: remove locked .vs files

---
+++ Practica1MVC/Models/ViewModels/ListaSucursales.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practica1MVC.Models.ViewModels
+{
+    public class ListaSucursales
+    {
+        public int SucursalId { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+    }
+}
---
+++ Practica1MVC/Models/ViewModels/Sucursal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practica1MVC.Models.ViewModels
+{
+    public class Sucursal : ListaSucursales
+    {
+        [Display (Name = "ID de la Sucursal")]
+
+        public int SucursalId { get; set; }
+
+        [Display(Name = "Direccion")]
+
+        public string Direccion { get; set; }
+
+
+    }
+}
